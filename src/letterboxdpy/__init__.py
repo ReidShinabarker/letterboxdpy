fd987a3fd7d910afe08756636f10edde556519cb
Reid Shinabarker <44105455+ReidShinabarker@users.noreply.github.com>
2023-11-30 01:38:16 -0500

    Commented out test code that's causing an issue

--- src/letterboxdpy/__init__.py
+++ src/letterboxdpy/__init__.py
@@ -1,6 +1,6 @@
-import user
-from user import User
-
-test = User("cinebarker")
-print(test.username)
-print(user.user_mutuals(test))
+# import user
+# from user import User
+#
+# test = User("cinebarker")
+# print(test.username)
+# print(user.user_mutuals(test))
